4e2071fbfcc860004a4ba69692258ebbd2e5aeec
Junsoo <junsoo@strivegaming.com>
2024-01-22 14:07:30 -0800

    Feat: Fetching Accounts from Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,20 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
+const web3 = new Web3(ganache.provider());
+const assert = require('assert');
 
-// updated ganache and web3 imports added for convenience
+let accounts;
 
 // contract test code will go here
+beforeEach(async() => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    //use one of those accounts to deploy
+    //the contract
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts);
+    })
+})
