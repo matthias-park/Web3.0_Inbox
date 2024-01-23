725214cf27c9133d93ccec9513865e48bfe65344
Junsoo <junsoo@strivegaming.com>
2024-01-23 12:49:25 -0800

    Feat: Testing Message Updates

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,6 +6,7 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!';
 
 // contract test code will go here
 beforeEach(async () => {
@@ -20,6 +21,17 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    })
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0], gas: '1000000'});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
     })
 })
