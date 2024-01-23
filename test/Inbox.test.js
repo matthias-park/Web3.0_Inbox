343165bb1fa49d02f5df59f4030886f997f32088
Junsoo <junsoo@strivegaming.com>
2024-01-23 12:12:40 -0800

    Feat: Deployment with Web3

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,19 +2,24 @@ const ganache = require('ganache');
 const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
 const assert = require('assert');
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 // contract test code will go here
-beforeEach(async() => {
+beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
     //use one of those accounts to deploy
     //the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' });
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     })
 })
