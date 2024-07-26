6a197433f137b3b97a92c12d3f2d79bbe7c436b4
Junsoo Park <matthiaspark0514@gmail.com>
2024-07-25 17:23:08 -0700

    Feat: Hide Mnemonic

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,9 +1,7 @@
+// linter warnings (red underline) about pragma version can igonored!
 pragma solidity ^0.4.17;
 
-// linter warnings (red underline) about pragma version can igonored!
-
 // contract code will go here
-
 contract Inbox {
     string public message;
 
--- deploy.js
+++ deploy.js
@@ -1,27 +1,28 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { interface, bytecode } = require("./compile");
+const mnemonic = process.env.MNEMONIC;
 
 const provider = new HDWalletProvider(
-    'benefit dress pattern culture wrong razor ivory stable obvious gesture reopen kiss',
-    'https://sepolia.infura.io/v3/136e7cebfea84fd7a9fcf2c3cfe4340b'
+  mnemonic,
+  "https://sepolia.infura.io/v3/136e7cebfea84fd7a9fcf2c3cfe4340b"
 );
 
 const web3 = new Web3(provider);
 
 // deploy code will go here
 
-const deploy = async ()  => {
-    const accounts = await web3.eth.getAccounts();
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
-    const res = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi There']})
-        .send({ from: accounts[0], gas: '1000000' })
+  const res = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi There"] })
+    .send({ from: accounts[0], gas: "1000000" });
 
-    console.log('Contract deployed to',res.options.address);
-    provider.engine.stop();
+  console.log("Contract deployed to", res.options.address);
+  provider.engine.stop();
 };
 
 deploy();
